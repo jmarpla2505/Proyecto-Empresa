a6eafd8627aed004829dfaca877e64eac6ccc574
Raul <al_rlopezm@iessierradelasvillas.org>
2023-02-17 11:12:40 +0100

    Funcionalidad Botón Finalizada

--- Proyecto Empresa.py
+++ Proyecto Empresa.py
@@ -1,4 +1,5 @@
 import flet as ft
+import time
 
 def main(page: ft.Page):
     page.title = "Proyecto Empresa"
@@ -9,25 +10,37 @@ def main(page: ft.Page):
     global contador
     txtcontador = ft.TextField(value=0)
 
-
     def funcionLogin(r):
+        vLista = []
+        p = open("UsuarioYContraseña.txt","r")
+        for linea in p:
+            lineasl = linea.replace("\n","")
+            vLista.append(lineasl)
+        p.close()        
+        #print (vLista)
         contador = txtcontador.value
-        usuariob = "Raul"
-        contraseñab = "123"
-        dlg = ft.AlertDialog(
-        title=ft.Text("Vuelvelo a intentar"))
-        page.dialog = dlg
-        if contador<3 or usuario.value != usuariob or contraseña.value != contraseñab:
-            dlg.open = True
-            contador = contador + 1
-            txtcontador.value = contador
-            print(contador)
-        elif contador >= 3:
-            print("d")
+        #vLista.count(0) == usuario
+        #vLista.count(1) == contraseña
+        if contador<3:
+            if vLista.count(usuario.value) == 0 or vLista.count(contraseña.value) == 0:
+                dlg = ft.AlertDialog(
+                title=ft.Text("Vuelvelo a intentar"))
+                page.dialog = dlg
+                dlg.open = True
+                contador = contador + 1
+                txtcontador.value = contador
+                print(contador)
+            else:
+                dlg = ft.AlertDialog(
+                title=ft.Text("Inicio de sesión correcto"))
+                page.dialog = dlg
+                dlg.open = True
+                page.clean()
         else:
-            print("a")
+                time.sleep(1)
+                page.window_close()
+                print("Intentos máximos realizados, intento de inicio de sesión fallido")
         page.update()
-        return contador
 
     img1 = ft.Image(
         src=f"Icono.png",
